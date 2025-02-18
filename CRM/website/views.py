ed32662808f422bfc9186fd2465200dbefd1d6c9
YasaraD <127465894+YasaraD@users.noreply.github.com>
2025-02-18 22:46:37 +0530

    add record

--- CRM/website/views.py
+++ CRM/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -56,4 +56,29 @@ def customer_record(request, pk):
 		return render(request, 'record.html', {'customer_record':customer_record})
 	else:
 		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+	
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
+
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
 		return redirect('home')
